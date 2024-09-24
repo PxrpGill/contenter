f4a32b85a856af21f87104729d79724ed6f59ad1
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2024-09-25 00:25:40 +0300

    feat: add circle stories

--- src/shared/ui/swiper-element/index.tsx
+++ src/shared/ui/swiper-element/index.tsx
@@ -13,8 +13,6 @@ interface ISwiperElement {
 export function SwiperElement({ title, description, img, action }: ISwiperElement) {
   return (
     <article className={styles.slide} onClick={action}>
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
       <img className={styles.image} src={img.src} alt={img.alt} draggable="false"/>
     </article>
   );
